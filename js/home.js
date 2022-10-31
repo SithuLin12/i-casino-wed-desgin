7d0e04f220f7038766843888487f19a24b38931b
sithulin <hack09715@gmail.com>
2022-11-01 03:32:00 +0630

    popup

--- js/home.js
+++ js/home.js
@@ -28,6 +28,82 @@ for(let i=0; i<marqueeElementsDisplayed; i++) {
 
 
 // btn active
+function toggleClass() {
+  document.getElementById("popup-1").classList.toggle('active');
+}
+
+function toggleClassKpay() {
+  document.getElementById("popup-2").classList.toggle('active');
+}
+
+function toggleClassWavepay() {
+  document.getElementById("popup-3").classList.toggle('active');
+}
+
+function toggleClassCbpay() {
+  document.getElementById("popup-4").classList.toggle('active');
+}
+
+function toggleClassOnepay() {
+  document.getElementById("popup-5").classList.toggle('active');
+}
+
+function toggleClassMytelpay() {
+  document.getElementById("popup-6").classList.toggle('active');
+}
+
+function toggleClassAyabank() {
+  document.getElementById("popup-7").classList.toggle('active');
+}
+
+function toggleClassKbzbank() {
+  document.getElementById("popup-8").classList.toggle('active');
+}
+
+function toggleClassYomabank() {
+  document.getElementById("popup-9").classList.toggle('active');
+}
+
+function toggleClassAgdbank() {
+  document.getElementById("popup-10").classList.toggle('active');
+}
+
+toggleClassChangePassword
+
+function toggleClassChangePassword() {
+  document.getElementById("popup-1").classList.toggle('active');
+}
+
+
+
+function toggleClassUpdateLevel2() {
+  document.getElementById("popup-2").classList.toggle('active');
+}
+
+function toggleClassEditusername() {
+  document.getElementById("popup-3").classList.toggle('active');
+}
+
+function toggleClassEditemail() {
+  document.getElementById("popup-4").classList.toggle('active');
+}
+
+function toggleClassEditphone() {
+  document.getElementById("popup-5").classList.toggle('active');
+}
+
+function toggleClassEditbirthdate() {
+  document.getElementById("popup-6").classList.toggle('active');
+}
+
+function toggleClassTransfer() {
+  document.getElementById("popup-27").classList.toggle('active');
+}
+
+// accountpay active
+
+
+
 
 
 
